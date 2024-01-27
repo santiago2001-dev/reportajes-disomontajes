17d422ad729d62e9abb36d37b590ede50888ca1d
victor morales <63662613+santiago2001-dev@users.noreply.github.com>
2024-01-27 12:53:51 -0500

    feat: crud users and selects

---
+++ src/reportes/leePlantilla.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reportes;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class leePlantilla {
+    
+    public void leerDoc (){
+    
+    
+    
+    
+    
+    }
+
+}
